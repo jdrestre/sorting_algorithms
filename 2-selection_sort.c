e82823567fb960c7dfe2233f851621dc154bdb2f
danielcinome <danielchinome987@gmail.com>
2020-02-01 15:10:26 -0500

    Task Complted

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+* selection_sort - sorts an array of integers in ascending order
+* @array : pointer array
+* @size : size
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	int val = 0, indx = 0, flag = 0;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		val = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (val > array[j])
+			{
+				indx = j;
+				val = array[j];
+				flag = 1;
+			}
+		}
+		array[indx] = array[i];
+		array[i] = val;
+		if (flag == 1)
+			print_array(array, size);
+		flag = 0;
+	}
+}
