ec1f16901f4f7e1c57ae53459ef635727b59c825
danielcinome <danielchinome987@gmail.com>
2020-02-05 21:21:03 +0000

    check

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -13,49 +13,34 @@ void cocktail_sort_list(listint_t **list)
 	if (list == NULL)
 		return;
 	head = *list;
-	while(state == 1)
+	while (state < 3)
 	{
-	while (head->next != NULL)
-	{
-		if (head->n > head->next->n)
+		while (head->next != NULL)
 		{
-			state = 1;
-			temp = head->next;
-			if (head->prev != NULL)
-				head->prev->next = temp;
-			if (temp->next != NULL)
-				temp->next->prev = head;
- 			temp->prev = head->prev;
-			head->next = temp->next;
-                temp->next = head;
-                if (head->prev == NULL)
-                    *list = temp;
-                head->prev = temp;
-            }
-            head = temp->next;
-		}
-        print_list(*list);
-        state = 0;
-        while (head->prev != NULL)
-        {
-            if (head->prev->n > head->n)
-            {
-                state = 1;
-                temp = head->prev;
-                if (temp->prev != NULL)
-                    temp->prev->next = head;
-                if (head->next != NULL)
-                    head->next->prev = temp;
-                head->prev = temp->prev;
-                temp->next = head->next;
-                temp->prev = head;
-                head->next = temp;
-            }
-            head = temp;
-            head = head->prev;
-            if (head->prev == NULL)
-                *list = head;
-            print_list(*list);
+            head = head->next;
         }
-    }
+		print_list(*list);
+		state++;
+		while (head->prev != NULL)
+		{
+			if (head->prev->n > head->n)
+			{
+				
+				temp = head->prev;
+				if (temp->prev != NULL)
+					temp->prev->next = head;
+				if (head->next != NULL)
+					head->next->prev = temp;
+				head->prev = temp->prev;
+				temp->next = head->next;
+				temp->prev = head;
+				head->next = temp;
+			}
+			head = temp;
+			head = head->prev;
+			if (head->prev == NULL)
+				*list = head;
+			print_list(*list);
+		}
+	}
 }
